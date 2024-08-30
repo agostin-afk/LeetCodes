a1bd56c76a748d148e0fc8102f83772d229aedbf
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-08-30 14:56:48 -0300

    Create longest_commom.py

---
+++ python/longest_commom.py
@@ -0,0 +1,33 @@
+"""
+Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+"""
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+    junta = strs[0]
+    for i in strs[1:]:
+        while i.find(junta) != 0:
+            junta = junta[:-1]
+            if not junta:
+                return ""
+    return junta
